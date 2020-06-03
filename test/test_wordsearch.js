08bc732a819de6f3d63e6fe948e9dbb2613b03d6
Sophie <sophiemargueritedubois@gmail.com>
2020-06-03 21:59:01 +0000

    Completed pair programming assignment

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -35,4 +35,49 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+  it("should return true if the word is present vertically", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'S', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'O', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'P', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'H', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'I', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'SOPHIE')
+
+    assert.isTrue(result);
+  });
+  it("should return true if the word is backwards present horizontally", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'S', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'O', 'N', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'P', 'A', 'U', 'A', 'L'],
+      ['H', 'B', 'R', 'E', 'G', 'M', 'A', 'N'],
+      ['W', 'H', 'C', 'I', 'G', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'E', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'R', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'B', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'NAMGERB')
+
+    assert.isTrue(result);
+  });
+  it("should return true if the word is backwards present vertically", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'S', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'O', 'N', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'P', 'A', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'H', 'M', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'I', 'G', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'E', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'R', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'B', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'BREGMAN')
+
+    assert.isTrue(result);
+  });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,37 @@
-const wordSearch = (letters, word) => { 
+const wordSearch = (letters, word) => {
+    return (checkHorizontal(letters, word) || checkVertical(letters, word) || checkBackwards(letters, word));
+}
+
+const checkHorizontal = function (letters, word) {
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
-}
+    return false;
+};
+
+const checkVertical = function (letters, word) {
+    let transposed = transpose(letters);
+    return checkHorizontal(transposed, word);
+};
+
+const checkBackwards = function (letters, word) {
+    let reversed = word.split('').reverse().join('');
+    return (checkHorizontal(letters, reversed) || checkVertical(letters, reversed))
+};
+
+const transpose = function (matrix) {
+    const solutionArray = [];
+    for (let i = 0; i < matrix[0].length; i++) {
+        const subArray = [];
+        for (let j = 0; j < matrix.length; j++) {
+            subArray.push(matrix[j][i]);
+        }
+        solutionArray.push(subArray);
+    }
+    return solutionArray;
+};
+
+
 
 module.exports = wordSearch
